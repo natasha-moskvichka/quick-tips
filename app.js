1ee96211a64e4619e3f92e3ebc09ca7f872fb50e
Natalia <y_natalya_v@mail.ru>
2024-08-14 16:28:46 +0300

    toggle theme

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-
-function formatMoney (value) {
+const formatMoney = (value) => {
     value = Math.ceil(value * 100) / 100;
     value = value.toFixed(2);
     return "₽ " + value;
 }
 
-function formatSplit (value) {
+const formatSplit = (value) => {
     if (value === "1") return value + " человека";
     if (value) return value + " человек";
     return value + " человека"
 }
-function  update () {
+const update = () => {
     const bill = Number(document.querySelector('#yourBill').value);
     const tipPercent = document.querySelector('#tipInput').value;
     const split = document.querySelector('#splitInput').value;
@@ -28,4 +27,41 @@ function  update () {
 }
 
 const container = document.querySelector('#container');
-container.addEventListener('input', update)
+container.addEventListener('input', update);
+
+const buttonThemeLight = document.querySelector('.theme-button-light');
+const buttonThemeDark = document.querySelector('.theme-button-dark');
+
+const toggleButtonActive = () => {
+    document.querySelector('.theme-button-active');
+    buttonThemeDark.classList.toggle('theme-button-active');
+    buttonThemeLight.classList.toggle('theme-button-active');
+}
+
+const buttonThemeLightHandler = () => {
+    toggleButtonActive();
+}
+
+const buttonThemeDarkHandler = () => {
+    toggleButtonActive();
+}
+
+const  init = () => {
+    buttonThemeLight.addEventListener('click', buttonThemeLightHandler);
+    buttonThemeDark.addEventListener('click', buttonThemeDarkHandler);
+};
+
+init();
+
+const toggleButton = document.querySelectorAll('.theme-button');
+
+toggleButton.forEach(function (button) {
+    button.addEventListener('click', function () {
+            let theme = document.documentElement.getAttribute('data-theme');
+            document.documentElement.setAttribute(
+                'data-theme',
+                theme === 'dark' ? 'light' : 'dark',
+            )
+        }
+    )
+})
